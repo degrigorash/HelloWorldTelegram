be6097ec129b919b508bdff5026c043fe2b3ded9
Igor Kozlenko <i.kozlenko@neemble.ru>
2016-07-14 20:23:53 +0300

    try

--- try.py
+++ try.py
@@ -7,4 +7,4 @@ BOT_TOKEN = '261616344:AAG7mO1GEA6KDwPYFV6V3hiXv1PMfKYlcvM'
 bot = telebot.TeleBot(BOT_TOKEN)
 
 if __name__ == '__main__':
-     bot.send_message('@helloworldru', 'Hello World!!!')
+     bot.sendMessage('@helloworldru', 'Hello World!!!')
