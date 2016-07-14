80b8b6b2d73126eadd50a7f4b0faaadb32cfa8a1
Igor Kozlenko <i.kozlenko@neemble.ru>
2016-07-14 20:28:31 +0300

    try

--- try.py
+++ try.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
-import config
 import telebot
 
-BOT_TOKEN = '261616344:AAG7mO1GEA6KDwPYFV6V3hiXv1PMfKYlcvM'
+API_TOKEN = '261616344:AAG7mO1GEA6KDwPYFV6V3hiXv1PMfKYlcvM'
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(API_TOKEN)
 
-if __name__ == '__main__':
-     bot.sendMessage('@helloworldru', 'Hello World!!!')
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
