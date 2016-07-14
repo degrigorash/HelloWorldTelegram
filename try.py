e96f361347e4844959e780443ec2dbba4f07bd48
Igor Kozlenko <i.kozlenko@neemble.ru>
2016-07-14 21:22:16 +0300

    try new

--- try.py
+++ try.py
@@ -1,45 +1,11 @@
+Вуд:
 # -*- coding: utf-8 -*-
-import logging
-import telegram
+from twx.botapi import TelegramBot
 
-update_id = None
+BOT_TOKEN = '261616344:AAG7mO1GEA6KDwPYFV6V3hiXv1PMfKYlcvM'
 
-def main():
-    global update_id
-    # Telegram Bot Authorization Token
-    bot = telegram.Bot('261616344:AAG7mO1GEA6KDwPYFV6V3hiXv1PMfKYlcvM')
-
-    # get the first pending update_id, this is so we can skip over it in case
-    # we get an "Unauthorized" exception.
-    try:
-        update_id = bot.getUpdates()[0].update_id
-    except IndexError:
-        update_id = None
-
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    while True:
-        try:
-            echo(bot)
-        except NetworkError:
-            sleep(1)
-        except Unauthorized:
-            # The user has removed or blocked the bot.
-            update_id += 1
-
-
-def echo(bot):
-    global update_id
-    # Request updates after the last update_id
-    for update in bot.getUpdates(offset=update_id, timeout=10):
-        # chat_id is required to reply to any message
-        chat_id = update.message.chat_id
-        update_id = update.update_id + 1
-
-        if update.message:  # your bot can receive updates without messages
-            # Reply to the message
-            bot.sendMessage(chat_id=chat_id, text=update.message.text)
-
+bot = TelegramBot(BOT_TOKEN)
 
 if __name__ == '__main__':
-    main()
+     a = bot.send_message(u'@helloworldru', u'Hellorld barowold!').wait()
+     print(a)
