b2cd5c421dd9810be798d1acedbc0efaaed3eee8
Igor Kozlenko <i.kozlenko@neemble.ru>
2016-07-14 20:19:50 +0300

    try

--- try.py
+++ try.py
@@ -2,7 +2,9 @@
 import config
 import telebot
 
-bot = telebot.TeleBot(config.token)
+BOT_TOKEN = '261616344:AAG7mO1GEA6KDwPYFV6V3hiXv1PMfKYlcvM'
+
+bot = telebot.TeleBot(BOT_TOKEN)
 
 if __name__ == '__main__':
      bot.send_message('@helloworldru', 'Hello World!!!')
